317fe61508ea334fa4c63a042041cdb17be11b16
Lasse Kauranen <lasse.kauranen@metropolia.fi>
2024-11-06 10:09:32 +0200

    Iteration 3 complete

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,39 +1,18 @@
-function Services() {
+import { services } from '../data'
+import Title from './Title'
+import Service from './Service'
+
+const Services = () => {
   return (
-    <section className="section" id="about">
-  <div className="section-title">
-    <h2>
-      about <span>us</span>
-    </h2>
-  </div>
-  <div className="section-center about-center">
-    <div className="about-img">
-      <img
-        src="images/about.jpeg"
-        className="about-photo"
-        alt="awesome beach"
-      />
-    </div>
-    <article className="about-info">
-      <h3>explore the difference</h3>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur
-        quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
-        unde dolor?
-      </p>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur
-        quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
-        unde dolor?
-      </p>
-      <a href="#" className="btn">
-        read more
-      </a>
-    </article>
-  </div>
-</section>
+    <section className='section services' id='services'>
+      <Title title='our' subTitle='services' />
 
-  );
+      <div className='section-center services-center'>
+        {services.map((service) => {
+          return <Service {...service} key={service.id} />
+        })}
+      </div>
+    </section>
+  )
 }
-
-export default Services;
+export default Services
